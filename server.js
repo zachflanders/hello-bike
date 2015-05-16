ffbec98535f39e7fed6898ae7a0b7860c3c3f2c1
Zach Flanders <zachflanders@gmail.com>
2015-05-16 17:43:56 -0500

    Adding more http methods to handle CRUD

--- server.js
+++ server.js
@@ -59,7 +59,42 @@ apiRouter.route('/users')
       }
       res.json({message: 'user created!'});
     });
+  })
+  .get(function(req, res){
+    User.find(function(err,users){
+      if(err) res.send(err);
+      res.json(users);
+    });
   });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+      if(err) res.send(err);
+      res.json(user);
+    });
+  })
+  .put(function(req, res){
+    User.findById(req.params.user_id, function (err, user){
+      if(err) res.send(err);
+      if(req.body.name) user.name = req.body.name;
+      if(req.body.username) user.username = req.body.username;
+      if(req.body.password) user.password = req.body.password;
+      user.save(function(err){
+        if(err) res.send(err);
+        res.json({message: 'User updated.'});
+      });
+    });
+  })
+  .delete(function(req, res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user){
+      if(err) res.send(err);
+      res.json({message:'Successfully deleted.'});
+    });
+  });
+
 
 adminRouter.get('/', function(req, res){
   res.send('I am the admin dashboard');
