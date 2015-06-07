1d4a9b5fc185f7a077eaef92e7b7c147ca9ff9f8
Zach Flanders <zachflanders@gmail.com>
2015-06-07 08:39:36 -0500

    created directive for adding routes to maps
    wow. so mappy. much lines.

--- public/app/controllers/activityCtrl.js
+++ public/app/controllers/activityCtrl.js
@@ -1,5 +1,5 @@
 angular.module('activityCtrl',['leaflet-directive', 'activityService'])
-  .controller('activityController', function(Activity, leafletData){
+  .controller('activityController',  function(Activity,  leafletData){
     var vm = this;
     vm.processing = true;
     console.log('hello');
@@ -9,24 +9,14 @@ angular.module('activityCtrl',['leaflet-directive', 'activityService'])
         vm.processing = false;
         // bind the users that come back to vm.users
         vm.activities = data;
-        console.log(vm.activities);
+
 
   });
-  angular.extend(vm, {
-      center: {
-          autoDiscover: true,
-          zoom: 16
-      },
-      controls: {
-          draw: {circle: false, rectangle: false, polygon: false, marker: false}
-      },
-      polyline: {
-        marker1:{lat:39.103, lng:-94.583, focus: true}
-      }
- });
 
 
- //L.polyline(vm.route).addTo(map);
+
+
+
   vm.deleteActivity =function(id){
     console.log('delete!');
     vm.processing=true;
@@ -40,6 +30,46 @@ angular.module('activityCtrl',['leaflet-directive', 'activityService'])
       });
   };
 })
+.directive('mapDirective', function() {
+// return the directive link function. (compile function not needed)
+return function(scope, element, attrs) {
+
+
+
+// watch the expression, and alert the class.
+//the directive has access to the whole element
+scope.$watch(attrs.mapDirective, function(value) {
+
+    var string = attrs.route;
+    var stringArray = string.slice(1,string.length-1);
+    console.log(stringArray, stringArray.indexOf(']'));
+    var stringArray2 = [];
+    while (stringArray.indexOf(']') > -1){
+      stringArray2.push(stringArray.slice(1,stringArray.indexOf(']')));
+      stringArray = stringArray.slice(stringArray.indexOf(']')+2, stringArray.length);
+    };
+    console.log(stringArray2);
+    var array = [];
+    stringArray2.forEach(function(entry){
+      var thing = JSON.parse("[" + entry + "]");
+      array.push(thing);
+    });
+    console.log(array);
+
+    var div = document.getElementById(attrs.id);
+    console.log(div);
+    var map = L.map(attrs.id).setView([51.505, -0.09], 13);
+
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    var polyline = L.polyline(array).addTo(map);
+});
+
+}
+})
+
 .controller('activityCreateController', function(Activity) {
   var vm = this;
 // variable to hide/show elements of the view // differentiates between create or edit pages
